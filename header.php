1c2544773d7e134e4ec475f7fb2cc4c62ac54082
Lordsamourai <157367778+Lordsamourai@users.noreply.github.com>
2024-09-25 15:23:02 +0200

    j

--- header.php
+++ header.php
@@ -1,13 +1,7 @@
-
-    <header class="header">
-        <div class="logo">
-            <img src="logo.png" alt="Logo" class="logo">
-        </div>
-        <h1 class="title">LIRA</h1>
-        <div class="buttons">
-            <form action="acceuil.php">
-            <button class="button">Accueil</button>
-            </form>
-            <button class="button">Déconnexion</button>
-        </div>
-    </header>
+<header class="header">
+    <h1 class="title">LIRA</h1>
+    <div class="header-buttons">
+        <button class="button">Accueil</button>
+        <button class="button">Déconnexion</button>
+    </div>
+</header>
