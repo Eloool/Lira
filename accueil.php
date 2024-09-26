8c4fdbf0f11afa6f31007e28af612ffc11146e32
Corentin75 <134609492+Corentin75@users.noreply.github.com>
2024-09-26 17:55:24 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet" media="all" type="text/css">
     <title>Accueil</title>
+    <?php include "db_connect.php";
+    include "header.php"; ?>
 </head>
 <body>
-    <p>Voici l'acceuil de Lira</p>
 </body>
 </html>
