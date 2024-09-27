24486898b1b0726ac7af63cbc855bb8b87a48cfb
Lordsamourai <157367778+Lordsamourai@users.noreply.github.com>
2024-09-27 11:55:10 +0200

    Update_Genocide_2

---
+++ test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="test.css" rel="stylesheet" media="all" type="text/css">
+    <title>Document</title>
+ 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="logo.png" sizes="32x32" href="logo.png">
+    <link rel="icon" type="logo.png" sizes="16x16" href="logo.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+</head>
+<body>
+    <?php 
+    include "header.php";
+    include "footer.php";
+    ?>
+</body>
+</html>
+
+
+
