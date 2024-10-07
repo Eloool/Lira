fe02c1722b881afb590bd730f086c5edd6b696b5
Corentin75 <134609492+Corentin75@users.noreply.github.com>
2024-10-07 20:06:15 +0200

    commentaires functions.php

--- functions.php
+++ functions.php
@@ -1,145 +1,110 @@
 <?php
+// fonction de connexion à la base
 function db_connect() {
-    // Informations de connexion
+    // infos de connexion
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "lira";
 
-    // Création de la connexion avec mysqli
+    // création de la connexion avec mysqli
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Vérification des erreurs de connexion
+    // on vérifie les possibles erreurs de connexion
     if ($conn->connect_error) {
         die("Échec de la connexion : " . $conn->connect_error);
     }
-
-    // Retourner l'objet de connexion si tout va bien
+    
     return $conn;
 }
 
+
+// fonction pour récupérer tous les projets
 function get_all_projects($conn) {
-    // Requête SQL
     $sql = "SELECT * FROM equipesprj";
 
-    // Préparation et exécution de la requête
     if ($stmt = $conn->prepare($sql)) {
         $stmt->execute();
-
-        // Récupération des résultats
         $result = $stmt->get_result();
-
-        // Récupération de tous les enregistrements sous forme de tableau associatif
         return $result->fetch_all(MYSQLI_ASSOC);
     } else {
-        // Gestion de l'erreur si la requête échoue
         die("Erreur dans la requête : " . $conn->error);
     }
 }
 
+
+// fonction pour récupérer les projets d'un utilisateur
 function get_user_projects($conn, $user_id) {
-    // Requête SQL avec un paramètre
     $sql = "SELECT * 
             FROM equipesprj
             JOIN rolesutilisateurprojet ON rolesutilisateurprojet.IdEq = equipesprj.IdEq
             WHERE rolesutilisateurprojet.IdU = ?";
 
-    // Préparation de la requête
     if ($stmt = $conn->prepare($sql)) {
-        // Liaison du paramètre (le ? correspond à $user_id)
         $stmt->bind_param('i', $user_id);
-
-        // Exécution de la requête
         $stmt->execute();
-
-        // Récupération des résultats
         $result = $stmt->get_result();
-
-        // Récupération des enregistrements sous forme de tableau associatif
         return $result->fetch_all(MYSQLI_ASSOC);
     } else {
-        // Gestion de l'erreur si la requête échoue
         die("Erreur dans la requête : " . $conn->error);
     }
 }
 
 
+// fonction pour récupérer les tâches d'un utilisateur
 function get_user_tasks($conn, $user_id) {
-    // Requête SQL pour récupérer les tâches d'un utilisateur donné
     $sql = "SELECT * 
             FROM taches
             JOIN sprintbacklog ON sprintbacklog.IdT = taches.IdT
             WHERE sprintbacklog.IdU = ?";
 
-    // Préparation de la requête
     if ($stmt = $conn->prepare($sql)) {
-        // Liaison du paramètre (le ? correspond à $user_id)
         $stmt->bind_param('i', $user_id);
-
-        // Exécution de la requête
         $stmt->execute();
-
-        // Récupération des résultats
         $result = $stmt->get_result();
-
-        // Récupération des enregistrements sous forme de tableau associatif
         return $result->fetch_all(MYSQLI_ASSOC);
     } else {
-        // Gestion de l'erreur si la requête échoue
         die("Erreur dans la requête : " . $conn->error);
     }
 }
 
+
+// fonction pour récupérer les tâches d'un projet
 function get_tasks_by_project($conn, $project_id) {
-    // Requête SQL pour récupérer les tâches liées à un projet spécifique
     $sql = "SELECT * 
             FROM taches
             JOIN sprintbacklog ON sprintbacklog.IdT = taches.IdT
             WHERE taches.IdEq = ?";
 
-    // Préparation de la requête
     if ($stmt = $conn->prepare($sql)) {
-        // Liaison du paramètre (le ? correspond à $project_id)
         $stmt->bind_param('i', $project_id);
-
-        // Exécution de la requête
         $stmt->execute();
-
-        // Récupération des résultats
         $result = $stmt->get_result();
-
-        // Récupération des enregistrements sous forme de tableau associatif
         return $result->fetch_all(MYSQLI_ASSOC);
     } else {
-        // Gestion de l'erreur si la requête échoue
         die("Erreur dans la requête : " . $conn->error);
     }
 }
 
+
+// fonction pour récupérer le rôle d'un utilisateur sur un projet
 function get_roles_for_user_for_project($conn,$user_id,$project_id){
     $sql = "SELECT IdR
             FROM rolesutilisateurprojet
             WHERE IdU = ? AND IdEq = ?";
 
-    // Préparation de la requête
     if ($stmt = $conn->prepare($sql)) {
-        // Liaison du paramètre (le ? correspond à $user_id)
         $stmt->bind_param('ii', $user_id, $project_id);
-
-        // Exécution de la requête
         $stmt->execute();
-
-        // Récupération des résultats
         $result = $stmt->get_result();
-
-        // Récupération des enregistrements sous forme de tableau associatif
         return $result->fetch_all(MYSQLI_ASSOC);
     } else {
-        // Gestion de l'erreur si la requête échoue
         die("Erreur dans la requête : " . $conn->error);
     }
 }
 
+
+// fonction pour récupérer les tâches d'un utilisateur sur un projet
 function get_tasks_for_user_by_project($conn,$user_id,$project_id){
     $sql = "SELECT TitreT,IdT
             FROM taches
@@ -147,65 +112,47 @@ function get_tasks_for_user_by_project($conn,$user_id,$project_id){
             JOIN rolesutilisateurprojet ON rolesutilisateurprojet.IdEq = equipesprj.IdEq
             WHERE rolesutilisateurprojet.IdU = ? AND equipesprj.IdEq = ?";
 
-    // Préparation de la requête
     if ($stmt = $conn->prepare($sql)) {
-        // Liaison du paramètre (le ? correspond à $user_id)
         $stmt->bind_param('ii', $user_id, $project_id);
-
-        // Exécution de la requête
         $stmt->execute();
-
-        // Récupération des résultats
         $result = $stmt->get_result();
-
-        // Récupération des enregistrements sous forme de tableau associatif
         return $result->fetch_all(MYSQLI_ASSOC);
     } else {
-        // Gestion de l'erreur si la requête échoue
         die("Erreur dans la requête : " . $conn->error);
     }
 }
+
+
+// fonction pour récupérer l'état des tâches
 function get_etats($conn){
     $sql = "SELECT DescEtat,IdEtat
             FROM etatstaches";
 
-    // Préparation de la requête
     if ($stmt = $conn->prepare($sql)) {
-        // Exécution de la requête
         $stmt->execute();
-
-        // Récupération des résultats
         $result = $stmt->get_result();
-
-        // Récupération des enregistrements sous forme de tableau associatif
         return $result->fetch_all(MYSQLI_ASSOC);
     } else {
-        // Gestion de l'erreur si la requête échoue
         die("Erreur dans la requête : " . $conn->error);
     }
 }
 
+
+// fonction pour récupérer l'état d'une tâche spécifique
 function get_etat_of_task($conn,$tache_id){
     $sql = "SELECT IdEtat
             FROM sprintbacklog
             WHERE IdT = ?";
 
-    // Préparation de la requête
     if ($stmt = $conn->prepare($sql)) {
-        // Liaison du paramètre (le ? correspond à $user_id)
         $stmt->bind_param('i', $tache_id);
-
-        // Exécution de la requête
         $stmt->execute();
-
-        // Récupération des résultats
         $result = $stmt->get_result();
-
-        // Récupération des enregistrements sous forme de tableau associatif
         return $result->fetch_all(MYSQLI_ASSOC)[0]['IdEtat'];
     } else {
-        // Gestion de l'erreur si la requête échoue
         die("Erreur dans la requête : " . $conn->error);
     }
 }
+
+// fin des fonctions
 ?>
