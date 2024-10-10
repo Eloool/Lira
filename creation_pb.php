4f58b0c2c2443e7337e2c9a3c556390944195694
Corentin75 <134609492+Corentin75@users.noreply.github.com>
2024-10-10 13:23:11 +0200

    Update creation_pb.php

--- creation_pb.php
+++ creation_pb.php
@@ -8,8 +8,19 @@
     <label for="userStory">User story :</label>
     <input type="text" id="userStory" name="userStory" placeholder="Décrivez l'user story">
 
-    <label for="idPrio">Priorité de la tâche :</label>
-    <input type="text" id="idPrio" name="idPrio" placeholder="Priorité de la tâche">
+    <label for="prio">Priorité de la tâche :</label>
+        <select name="prio" id="prio" required>
+            <?php
+            // on récupère la liste des priorités dans la base de données
+            $prioQuery = "SELECT idPriorite, DescPriorite, valPriorite FROM prioritestaches";
+            $prioResult = $conn->query($prioQuery);
+
+            // on affiche la liste
+            while ($row = $prioResult->fetch_assoc()) {
+                echo "<option value='{$row['idPriorite']}'>{$row['DescPriorite']}</option>";
+            }
+            ?>
+        </select>
 
     <input type="submit" value="Valider">
 </form>
@@ -19,19 +30,17 @@
     $userStory = $_POST['userStory'] ?? null;
     $idEq = isset($_GET['id']) ? (int)$_GET['id'] : 0;
     $idcout = 1;
-    $idPrio = $_POST['idPrio'] ?? null;
-    if (!strlen($idPrio)) $idPrio = "null";
+    $prio = $_POST['prio'] ?? null;
 
-    if ($titre !== null && $userStory !== null && $idPrio !== null) {
+    if ($titre !== null && $userStory !== null) {
         $titre = $conn->real_escape_string($titre);
         $userStory = $conn->real_escape_string($userStory);
-        $idPrio = $conn->real_escape_string($idPrio);
         // requête d'insertion dans le PB
         $sql = "INSERT INTO taches (TitreT, UserStoryT, IdEq, IdCout, IdPriorite)
-                VALUES ('$titre', '$userStory', '$idEq', '$idcout', $idPrio)";
+                VALUES ('$titre', '$userStory', '$idEq', '$idcout', $prio)";
+
         if ($conn->query($sql) !== TRUE) {
             echo "Erreur lors de l'ajout : " . $conn->error;
         }
     }
 ?>
-
