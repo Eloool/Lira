a5a9426a9855584f34e0c08c614e2c71c0dba6ac
Corentin75 <134609492+Corentin75@users.noreply.github.com>
2024-10-10 10:28:36 +0200

    Update creation_pb.php

--- creation_pb.php
+++ creation_pb.php
@@ -8,9 +8,6 @@
     <label for="userStory">User story :</label>
     <input type="text" id="userStory" name="userStory" placeholder="Décrivez l'user story">
 
-    <label for="cout">Coût de la tâche :</label>
-    <input type="text" id="cout" name="cout" placeholder="Coût estimé de la tâche">
-
     <label for="idPrio">Priorité de la tâche :</label>
     <input type="text" id="idPrio" name="idPrio" placeholder="Priorité de la tâche">
 
@@ -21,19 +18,17 @@
     $titre = $_POST['titre'] ?? null;
     $userStory = $_POST['userStory'] ?? null;
     $idEq = isset($_GET['id']) ? (int)$_GET['id'] : 0;
-    $cout = $_POST['cout'] ?? null;
+    $idcout = 1;
     $idPrio = $_POST['idPrio'] ?? null;
-    if (!strlen($cout)) $cout = "null";
     if (!strlen($idPrio)) $idPrio = "null";
 
-    if ($titre !== null && $userStory !== null && $cout !== null && $idPrio !== null) {
+    if ($titre !== null && $userStory !== null && $idPrio !== null) {
         $titre = $conn->real_escape_string($titre);
         $userStory = $conn->real_escape_string($userStory);
-        $cout = $conn->real_escape_string($cout);
         $idPrio = $conn->real_escape_string($idPrio);
         // requête d'insertion dans le PB
         $sql = "INSERT INTO taches (TitreT, UserStoryT, IdEq, IdCout, IdPriorite)
-                VALUES ('$titre', '$userStory', '$idEq', '$cout', $idPrio)";
+                VALUES ('$titre', '$userStory', '$idEq', '$idcout', $idPrio)";
         if ($conn->query($sql) !== TRUE) {
             echo "Erreur lors de l'ajout : " . $conn->error;
         }
